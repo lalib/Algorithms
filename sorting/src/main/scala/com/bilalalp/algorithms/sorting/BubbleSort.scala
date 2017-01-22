f3aec1b6e9492d2779211e216709e6b6024a4446
balp <bilal.alp@zalando.de>
2017-01-22 19:26:09 +0100

    Bubble sort added.

---
+++ sorting/src/main/scala/com/bilalalp/algorithms/sorting/BubbleSort.scala
@@ -0,0 +1,22 @@
+package com.bilalalp.algorithms.sorting
+
+object BubbleSort {
+
+  def sort[T](elementArray: Array[T])(implicit ordering: Ordering[T]): Unit = {
+
+    var swapped: Boolean = true
+
+    while (swapped) {
+
+      swapped = false
+      for (i <- 0 until elementArray.length - 1) {
+        if (ordering.compare(elementArray(i), elementArray(i + 1)) > 0) {
+          swapped = true
+          val tempValue: T = elementArray(i)
+          elementArray(i) = elementArray(i + 1)
+          elementArray(i + 1) = tempValue
+        }
+      }
+    }
+  }
+}
